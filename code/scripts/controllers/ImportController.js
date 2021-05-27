ebe77783300ee70f21c3d47e49d1d4605452e105
mastaleru <mastaleru.rafael@gmail.com>
2021-05-27 19:10:07 +0300

    import page from external file wip

---
+++ code/scripts/controllers/ImportController.js
@@ -0,0 +1,83 @@
+const { WebcController } = WebCardinal.controllers;
+const mappings = require("epi-utils").loadApi("mappings");
+const messages = [{
+    "messageType" : "Product",
+
+    "messageTypeVersion" : 0.1,
+    "senderId" : "NOVARTIS_P75_010",
+    "receiverId" : "ePI_DSU_NOVARTIS",
+    "messageId" : "00012856374589",
+    "messageDateTime" : "2021-04-27T10:12:12CET",
+    "product" : {
+        "productCode" : "07612791468124",
+        "internalMaterialCode" : "100200",
+        "inventedName" : "Ritalin",
+        "nameMedicinalProduct" : "Ritalin LA HGC 40mg 1x30",
+        "strength" : "40mg",
+        "flagEnableAdverseEventReporting" : false,
+        "adverseEventReportingURL" : "",
+        "flagEnableACFProductCheck" : false,
+        "acfProductCheckURL" : "",
+        "flagDisplayEPI_BatchRecalled" : false,
+        "flagDisplayEPI_SNRecalled" : true,
+        "flagDisplayEPI_SNDecommissioned" : true,
+        "flagDisplayEPI_SNUnknown" : true,
+        "flagDisplayEPI_EXPIncorrect" : true,
+        "flagDisplayEPI_BatchExpired" : true,
+        "patientSpecificLeaflet" : "",
+        "healthcarePractitionerInfo" : "",
+        "markets" : [
+            {
+                "marketId" : "DE",
+                "nationalCode" : "1234567",
+                "mahName" : "Novartis",
+                "legalEntityName" : "Novartis Deutschland AG"
+            },
+            {
+                "marketId" : "AT",
+                "nationalCode" : "23456",
+                "mahName" : "Novartis",
+                "legalEntityName" : "Novartis Österreich AG"
+            }
+        ]
+    }
+}];
+const model = {
+    filesChooser: {
+        label: "Select files",
+        accept: "json",
+        listFiles: true,
+        filesAppend: true,
+        files: []
+    }
+}
+export default class batchesController extends WebcController {
+    constructor(...props) {
+        super(...props);
+
+        this.model = this.setModel(model);
+        this.model.onChange('filesChooser', () => {
+            let filesArray = this.model.filesChooser.files || [];
+        });
+        this.on('add-file-folder', (event) => {
+            let filesArray = event.data || [];
+        });
+        //TODO extract if... look into MangeProductController
+        const holderInfo = {domain:"epi", subdomain:"default"};
+        const mappingEngine = mappings.getEPIMappingEngine(this.DSUStorage,{holderInfo:holderInfo});
+
+        mappingEngine.digestMessages(messages).then(undigestedMessages=>{
+
+            console.log(undigestedMessages);
+
+
+
+
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    }
+}
+
+
