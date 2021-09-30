45551c91dc0add2da90a797c510bcb8517afc241
skutner <sava.dumitru.daniel@gmail.com>
2021-09-30 15:45:29 +0300

    converted file paths to relative paths

--- code/scripts/utils.js
+++ code/scripts/utils.js
@@ -193,7 +193,7 @@ function timeAgo(time) {
 
 async function getUserDetails() {
     try {
-        const response = await fetch("/api-standard/user-details");
+        const response = await fetch("./api-standard/user-details");
         return await response.json();
     } catch (err) {
         console.error(`Failed to get user's details`, err);
