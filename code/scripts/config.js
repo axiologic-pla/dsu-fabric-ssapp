af13c5c7e4c904e5365633fbb00beafc41c79c94
adrian.ganga <adiganga2002@gmail.com>
2022-09-20 10:16:50 +0300

    #685 FS-SEC-004 - System provides report on user access

--- code/scripts/config.js
+++ code/scripts/config.js
@@ -17,6 +17,61 @@ async function initializeWebCardinalConfig() {
 const config = await initializeWebCardinalConfig();
 setConfig(config);
 
+addHook("afterAppLoads", async () => {
+  const openDSU = require("opendsu");
+  const w3cdid = openDSU.loadAPI("w3cdid");
+  const scAPI = openDSU.loadAPI("sc");
+  const groupDIDDocument = await $$.promisify(w3cdid.resolveDID)("did:ssi:group:vault:ePI_Administration_Group");
+  let adminUserList;
+  const dsuMainEnclave = await $$.promisify(scAPI.getMainEnclave)();
+  let did;
+  try {
+    did = await $$.promisify(dsuMainEnclave.readKey)("did");
+  } catch (e) {
+    console.log("Failed to read DID for logging logged in user ", e);
+    return;
+  }
+
+  try {
+    adminUserList = await $$.promisify(groupDIDDocument.listMembersByIdentity)();
+    const memberDID_Document = await $$.promisify(w3cdid.resolveDID)(did);
+    const msg = {
+      messageType: "UserLogin",
+      userDID: did,
+      userType: "epiWrite",
+      messageId: `${new Date().getTime()}|${did}`
+    };
+    for (let i = 0; i < adminUserList.length; i++) {
+      let adminDID_Document = await $$.promisify(w3cdid.resolveDID)(adminUserList[i]);
+      await $$.promisify(memberDID_Document.sendMessage)(JSON.stringify(msg), adminDID_Document);
+    }
+  } catch (e) {
+    console.log("Error sending login message to admins: ", e);
+  }
+
+  /*
+  // log in dsu audit log user login action
+
+  const LogService = require("gtin-resolver").loadApi("services").LogService;
+  let logService = new LogService();
+  const userDetails = await utils.getUserDetails();
+  console.log("Could not update audit log for user login. ");
+    return logService.log({
+      logInfo: {
+        name: userDetails.username,
+        email: userDetails.company
+      },
+      username: userDetails.username,
+      reason: `Logged in`,
+      metadata: "Logged in"
+    }, (err, result) => {
+      if (err) {
+        console.log("Could not update audit log for user login. ", err);
+      }
+      return;
+    })*/
+})
+
 addHook('beforePageLoads', 'generate-did', () => {
   WebCardinal.root.disableHeader = true;
 });
